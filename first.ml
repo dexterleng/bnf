5e701abd9a8e0152e6c929199137723ae40c6e77
Dexter Leng <dexterlengcs@gmail.com>
2020-04-12 15:31:29 +0800

    Change generate_first_set function definition order

--- first.ml
+++ first.ml
@@ -8,20 +8,26 @@ let find_production (assignments: Types.assignment list) (non_terminal: Types.no
 
 let epsilon_only_set = Types.FirstSet.add Types.FirstSet.empty Epsilon
 
-let rec generate_first_set_term (lines: Types.assignment list) (term: Types.term) = match term with
-  | NonTerminal(non_terminal) ->
-    let production = find_production lines non_terminal in
-    generate_first_set_assignment lines production
-  | Terminal(terminal) ->
-    Types.FirstSet.add Types.FirstSet.empty (Terminal terminal)
-  | Epsilon -> epsilon_only_set
+let rec generate_first_set_assignment (lines: Types.assignment list) (line: Types.assignment) =
+  let rhs = line.rhs in
+  generate_first_set_or_expr lines rhs
 
 and
 
-generate_first_set_primary_expr (lines: Types.assignment list) (primary_expr: Types.primary_expr) =
-  match primary_expr with
-    | PRIMARY_EXPR(term) -> generate_first_set_term lines term
-    | PRIMARY_PARENTHESIZED_EXPR(expr) -> generate_first_set_expr lines expr
+generate_first_set_expr (lines: Types.assignment list) (expr: Types.expr) =
+  generate_first_set_or_expr lines expr
+
+and
+
+generate_first_set_or_expr (lines: Types.assignment list) (or_expr: Types.or_expr) =
+  match or_expr with
+    | OR_EXPR(sequential_expr, or_expr) ->
+      let seq_first_set = generate_first_set_seq_expr lines sequential_expr in
+      let or_first_set = generate_first_set_or_expr lines or_expr in
+      Types.FirstSet.union (seq_first_set) (or_first_set)
+    | OR_EXPR_BASE(sequential_expr) ->
+      let seq_first_set = generate_first_set_seq_expr lines sequential_expr in
+      seq_first_set
 
 and
 
@@ -39,23 +45,17 @@ generate_first_set_seq_expr (lines: Types.assignment list) (seq_expr: Types.sequ
 
 and
 
-generate_first_set_or_expr (lines: Types.assignment list) (or_expr: Types.or_expr) =
-  match or_expr with
-    | OR_EXPR(sequential_expr, or_expr) ->
-      let seq_first_set = generate_first_set_seq_expr lines sequential_expr in
-      let or_first_set = generate_first_set_or_expr lines or_expr in
-      Types.FirstSet.union (seq_first_set) (or_first_set)
-    | OR_EXPR_BASE(sequential_expr) ->
-      let seq_first_set = generate_first_set_seq_expr lines sequential_expr in
-      seq_first_set
+generate_first_set_primary_expr (lines: Types.assignment list) (primary_expr: Types.primary_expr) =
+  match primary_expr with
+    | PRIMARY_EXPR(term) -> generate_first_set_term lines term
+    | PRIMARY_PARENTHESIZED_EXPR(expr) -> generate_first_set_expr lines expr
 
 and
 
-generate_first_set_expr (lines: Types.assignment list) (expr: Types.expr) =
-  generate_first_set_or_expr lines expr
-
-and
-
-generate_first_set_assignment (lines: Types.assignment list) (line: Types.assignment) =
-  let rhs = line.rhs in
-  generate_first_set_or_expr lines rhs
+generate_first_set_term (lines: Types.assignment list) (term: Types.term) = match term with
+  | NonTerminal(non_terminal) ->
+    let production = find_production lines non_terminal in
+    generate_first_set_assignment lines production
+  | Terminal(terminal) ->
+    Types.FirstSet.add Types.FirstSet.empty (Terminal terminal)
+  | Epsilon -> epsilon_only_set
