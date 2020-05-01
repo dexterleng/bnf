b1b4a85cf512e24fc7341f3d4bf8d4b1faeb299c
Dexter Leng <dexterlengcs@gmail.com>
2020-05-01 15:31:57 +0800

    Simplify follow set propagated return values

--- follow.ml
+++ follow.ml
@@ -3,7 +3,6 @@ open Types
 
 type return_value = {
   follow_set_map: FollowSet.t NonTerminalMap.t;
-  next_first: FirstSet.t;
   next_follow: FollowSet.t;
 }
 
@@ -28,26 +27,25 @@ generate_follow_sets (assignments: assignment list) (first_set_map: FirstSet.t N
     ~f:(fun map assignment -> NonTerminalMap.set map ~key: (NonTerminal assignment.lhs) ~data: FollowSet.empty)
     ~init: NonTerminalMap.empty
   in
-  let map = ref init_map in
+  let follow_set_map = ref init_map in
   let changed = ref true in
   while !changed do
-    let new_map = List.fold_left assignments ~init: !map ~f:(fun map assignment ->
+    let new_follow_set_map = List.fold_left assignments ~init: !follow_set_map ~f:(fun map assignment ->
       let result = generate_follow_set_assignment assignment first_set_map map in
       result.follow_set_map
     )
     in
 
-    changed := not (NonTerminalMap.equal (FollowSet.equal) !map new_map);
-    map := new_map;
+    changed := not (NonTerminalMap.equal (FollowSet.equal) !follow_set_map new_follow_set_map);
+    follow_set_map := new_follow_set_map;
   done;
-  !map
+  !follow_set_map
 
 and
 
 generate_follow_set_assignment (assignment: assignment) (first_set_map: FirstSet.t NonTerminalMap.t) (follow_set_map: FollowSet.t NonTerminalMap.t) =
   let follow_of_lhs = NonTerminalMap.find_exn follow_set_map (NonTerminal assignment.lhs) in
-  let epsilon_first_set = FirstSet.add FirstSet.empty Epsilon in
-  generate_follow_set_or_expr assignment.rhs first_set_map follow_set_map epsilon_first_set follow_of_lhs
+  generate_follow_set_or_expr assignment.rhs first_set_map follow_set_map follow_of_lhs
 
 and
 
@@ -55,9 +53,8 @@ generate_follow_set_expr
   (expr: expr)
   (first_set_map: FirstSet.t NonTerminalMap.t)
   (follow_set_map: FollowSet.t NonTerminalMap.t)
-  (next_first: FirstSet.t)
   (next_follow: FollowSet.t)
-  = generate_follow_set_or_expr expr first_set_map follow_set_map next_first next_follow
+  = generate_follow_set_or_expr expr first_set_map follow_set_map next_follow
 
 and
 
@@ -65,101 +62,84 @@ generate_follow_set_or_expr
   (or_expr: or_expr)
   (first_set_map: FirstSet.t NonTerminalMap.t)
   (follow_set_map: FollowSet.t NonTerminalMap.t)
-  (next_first: FirstSet.t)
   (next_follow: FollowSet.t)
   = match or_expr with
     | OR_EXPR_BASE(seq_expr) ->
       generate_follow_set_seq_expr
-        seq_expr first_set_map follow_set_map next_first next_follow
+        seq_expr first_set_map follow_set_map next_follow
     | OR_EXPR(seq_expr, or_expr) ->
       let result = generate_follow_set_or_expr
-        or_expr first_set_map follow_set_map next_first next_follow
+        or_expr first_set_map follow_set_map next_follow
       in
       generate_follow_set_seq_expr
-        seq_expr first_set_map result.follow_set_map next_first next_follow
+        seq_expr first_set_map result.follow_set_map next_follow
 and
 
 generate_follow_set_seq_expr
   (seq_expr: sequential_expr)
   (first_set_map: FirstSet.t NonTerminalMap.t)
   (follow_set_map: FollowSet.t NonTerminalMap.t)
-  (next_first: FirstSet.t)
   (next_follow: FollowSet.t)
   = match seq_expr with
     | SEQUENTIAL_EXPR_BASE(primary_expr) ->
       generate_follow_set_pri_expr
-        primary_expr true first_set_map follow_set_map next_first next_follow
+        primary_expr first_set_map follow_set_map next_follow
     | SEQUENTIAL_EXPR(primary_expr, seq_expr) ->
       let result = generate_follow_set_seq_expr
-        seq_expr first_set_map follow_set_map next_first next_follow
+        seq_expr first_set_map follow_set_map next_follow
       in
       generate_follow_set_pri_expr
-        primary_expr false first_set_map result.follow_set_map result.next_first result.next_follow
+        primary_expr first_set_map result.follow_set_map result.next_follow
 
 and
 
 generate_follow_set_pri_expr
   (primary_expr: primary_expr)
-  (seq_end: bool)
   (first_set_map: FirstSet.t NonTerminalMap.t)
   (follow_set_map: FollowSet.t NonTerminalMap.t)
-  (next_first: FirstSet.t)
   (next_follow: FollowSet.t)
   = match primary_expr with
     | PRIMARY_EXPR(term) ->
       generate_follow_set_term
-        term seq_end first_set_map follow_set_map next_first next_follow
+        term first_set_map follow_set_map next_follow
     | PRIMARY_PARENTHESIZED_EXPR(expr) ->
       generate_follow_set_expr
-        expr first_set_map follow_set_map next_first (follow_of_first next_first)
+        expr first_set_map follow_set_map next_follow
 
 and
 
 generate_follow_set_term
   (term: term)
-  (seq_end: bool)
   (first_set_map: FirstSet.t NonTerminalMap.t)
   (follow_set_map: FollowSet.t NonTerminalMap.t)
-  (next_first: FirstSet.t)
   (next_follow: FollowSet.t)
   = match term with
     | Terminal(terminal) ->
-    {
-      follow_set_map;
-      next_first = FirstSet.of_list [Terminal terminal];
-      next_follow = FollowSet.of_list [Terminal(terminal)];
-    }
+      {
+        follow_set_map;
+        next_follow = FollowSet.of_list [Terminal(terminal)];
+      }
     | NonTerminal(non_terminal) ->
       let new_follow_set_map =
-        (* add next_follow to follow set if non-terminal is last in sequence,
-          else, add next_first
-         *)
         let follow_set = NonTerminalMap.find_exn follow_set_map (NonTerminal non_terminal) in
-        if seq_end then
-          let new_follow_set = FollowSet.union follow_set (next_follow) in
-          NonTerminalMap.set follow_set_map ~key: (NonTerminal non_terminal) ~data: new_follow_set
-        else
-          let new_follow_set = FollowSet.union follow_set (follow_of_first next_first) in
-          NonTerminalMap.set follow_set_map ~key: (NonTerminal non_terminal) ~data: new_follow_set
+        let new_follow_set = FollowSet.union follow_set (next_follow) in
+        NonTerminalMap.set follow_set_map ~key: (NonTerminal non_terminal) ~data: new_follow_set
       in
 
       let first_set = NonTerminalMap.find_exn first_set_map (NonTerminal non_terminal) in
       let does_first_set_contain_epsilon = FirstSet.mem first_set Epsilon in
 
-      let new_next_first = if does_first_set_contain_epsilon
-        then FirstSet.union first_set next_first
-        else first_set
+      let new_next_follow = if does_first_set_contain_epsilon
+        then FollowSet.union next_follow (follow_of_first first_set)
+        else follow_of_first first_set
       in
-
       {
         follow_set_map = new_follow_set_map;
-        next_first = new_next_first;
-        next_follow;
+        next_follow = new_next_follow;
       }
     | Epsilon ->
       {
         follow_set_map;
-        next_first;
         next_follow;
       }
       
