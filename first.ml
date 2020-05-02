ee3f61cc709163a08e7c76a2577fdf25c09fc8ba
Dexter Leng <dexterlengcs@gmail.com>
2020-05-02 21:58:05 +0800

    Keep track of first sets of each alternation

--- first.ml
+++ first.ml
@@ -1,79 +1,83 @@
 open Core
 open Types
 
+type first_set_of_assignment = {
+  first_sets: FirstSet.t list;
+  union_of_first_sets: FirstSet.t;
+}
 
 let find_production (assignments: assignment list) (non_terminal: non_terminal) =
   List.find_exn assignments ~f:(fun assignment -> String.(=) assignment.lhs non_terminal)
 
 let epsilon_only_set = FirstSet.add FirstSet.empty Epsilon
 
-let rec generate_first_set_assignment (assignment: assignment) (first_set_map) =
-  let rhs = assignment.rhs in
-  let lhs = assignment.lhs in
-  let (new_map, _) = generate_first_set_or_expr lhs rhs first_set_map in
-  new_map
-and
+let rec 
 
-generate_first_set_expr (lhs: non_terminal) (expr: expr) (first_set_map: FirstSet.t NonTerminalMap.t) =
-  generate_first_set_or_expr lhs expr first_set_map
-
-and
-
-generate_first_set_or_expr (lhs: non_terminal) (or_expr: or_expr) (first_set_map: FirstSet.t NonTerminalMap.t) =
-  match or_expr with
-    | OR_EXPR(sequential_expr, or_expr) ->
-    (* UNION! *)
-      let (seq_first_set_map, _) = generate_first_set_seq_expr lhs sequential_expr first_set_map in
-      generate_first_set_or_expr lhs or_expr seq_first_set_map
-    | OR_EXPR_BASE(sequential_expr) ->
-      generate_first_set_seq_expr lhs sequential_expr first_set_map
-
-and
-
-generate_first_set_seq_expr (lhs: non_terminal) (seq_expr: sequential_expr) (first_set_map: FirstSet.t NonTerminalMap.t) =
-  match seq_expr with
-    | SEQUENTIAL_EXPR(term, sequential_expr) ->
-      (* UNION until non-epsilonable *)
-      let (term_first_set_map, epsilonable) = generate_first_set_term lhs term first_set_map in
-      if epsilonable then
-        generate_first_set_seq_expr lhs sequential_expr term_first_set_map
-      else
-        (term_first_set_map, false)
-    | SEQUENTIAL_EXPR_BASE(term) ->
-      generate_first_set_term lhs term first_set_map
-
-and
-
-generate_first_set_term (lhs: non_terminal) (term: term) (first_set_map: FirstSet.t NonTerminalMap.t) = match term with
-  | NonTerminal(non_terminal) ->
-    let non_terminal_first_set = NonTerminalMap.find_exn first_set_map (NonTerminal non_terminal) in
-    let lhs_first_set = NonTerminalMap.find_exn first_set_map (NonTerminal lhs) in
-    let new_lhs_first_set = FirstSet.union non_terminal_first_set lhs_first_set in
-    let new_map = NonTerminalMap.set first_set_map ~key:(NonTerminal lhs) ~data:new_lhs_first_set in
-    let epsilonable = FirstSet.mem non_terminal_first_set Epsilon in
-    (new_map, epsilonable)
-  | Terminal(terminal) ->
-    let terminal_first_set = FirstSet.add (FirstSet.empty) (Terminal terminal) in
-    let lhs_first_set = NonTerminalMap.find_exn first_set_map (NonTerminal lhs) in
-    let new_lhs_first_set = FirstSet.union terminal_first_set lhs_first_set in
-    let new_map = NonTerminalMap.set first_set_map ~key:(NonTerminal lhs) ~data:new_lhs_first_set in
-    (new_map, false)
-  | Epsilon ->
-    let lhs_first_set = NonTerminalMap.find_exn first_set_map (NonTerminal lhs) in
-    let new_lhs_first_set = FirstSet.add lhs_first_set Epsilon in
-    let new_map = NonTerminalMap.set first_set_map ~key:(NonTerminal lhs) ~data:new_lhs_first_set in
-    (new_map, true)
-
-let generate_first_sets (assignments: assignment list) =
+generate_first_sets (assignments: assignment list) =
   let init_map = List.fold_left assignments
-    ~f:(fun map assignment -> NonTerminalMap.add_exn map ~key: (NonTerminal assignment.lhs) ~data: FirstSet.empty)
+    ~f:(fun map assignment -> NonTerminalMap.add_exn map
+      ~key: (NonTerminal assignment.lhs)
+      ~data: { first_sets = []; union_of_first_sets = FirstSet.empty }
+    )
     ~init: NonTerminalMap.empty
   in
   let map = ref init_map in
   let changed = ref true in
   while !changed do
     let new_map = List.fold_left assignments ~init: !map ~f:(fun map assignment -> generate_first_set_assignment assignment map) in
-    changed := not (NonTerminalMap.equal (FirstSet.equal) !map new_map);
+    changed := not (NonTerminalMap.equal (fun a b -> FirstSet.equal a.union_of_first_sets b.union_of_first_sets) !map new_map);
     map := new_map;
   done;
   !map
+
+and
+
+generate_first_set_assignment (assignment: assignment) (first_set_map: first_set_of_assignment NonTerminalMap.t) =
+  let lhs = assignment.lhs in
+  let rhs = assignment.rhs in
+  let first_set_of_assignment = generate_first_set_or_expr rhs [] first_set_map in
+  NonTerminalMap.set first_set_map ~key: (NonTerminal lhs) ~data: first_set_of_assignment
+
+and
+
+generate_first_set_or_expr (or_expr: or_expr) (seq_expr_first_sets_rev: FirstSet.t list) (first_set_map: first_set_of_assignment NonTerminalMap.t) =
+  match or_expr with
+    | OR_EXPR(sequential_expr, or_expr) ->
+      let seq_first_set = generate_first_set_seq_expr sequential_expr first_set_map in
+      generate_first_set_or_expr or_expr (seq_first_set::seq_expr_first_sets_rev) first_set_map
+    | OR_EXPR_BASE(sequential_expr) ->
+      let seq_first_set = generate_first_set_seq_expr sequential_expr first_set_map in
+      let seq_expr_first_sets = List.rev (seq_first_set::seq_expr_first_sets_rev) in
+      let unions_of_first_sets = FirstSet.union_list seq_expr_first_sets in
+      { first_sets = seq_expr_first_sets; union_of_first_sets = unions_of_first_sets }
+
+and
+
+generate_first_set_seq_expr (seq_expr: sequential_expr) (first_set_map: first_set_of_assignment NonTerminalMap.t) =
+  match seq_expr with
+    | SEQUENTIAL_EXPR(term, sequential_expr) ->
+      (* UNION until non-epsilonable *)
+      let term_first_set = generate_first_set_term term first_set_map in
+      let does_first_set_contain_epsilon = FirstSet.mem term_first_set Epsilon in
+
+      if does_first_set_contain_epsilon then
+        FirstSet.union
+          term_first_set
+          (generate_first_set_seq_expr sequential_expr first_set_map)
+      else
+        term_first_set
+    | SEQUENTIAL_EXPR_BASE(term) ->
+      let term_first_set = generate_first_set_term term first_set_map in
+      term_first_set
+
+and
+
+generate_first_set_term (term: term) (first_set_map: first_set_of_assignment NonTerminalMap.t) = match term with
+  | NonTerminal(non_terminal) ->
+    let non_terminal_first_set_of_assignment = NonTerminalMap.find_exn first_set_map (NonTerminal non_terminal) in
+    non_terminal_first_set_of_assignment.union_of_first_sets
+  | Terminal(terminal) ->
+    let terminal_first_set = FirstSet.add (FirstSet.empty) (Terminal terminal) in
+    terminal_first_set
+  | Epsilon ->
+    epsilon_only_set
